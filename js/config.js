a05e0cd3c04bd50f8907377f51c65a38524b07dc
Riccardo Volpato <riccardo.volpato@gmail.com>
2020-05-09 12:14:50 +0100

    AWS WildRydes tutorial, pushing project files

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_EBM5jBTR5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7c4r3vpmb8nqqmh1p60fsoikrl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://8j955rx6f9.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
